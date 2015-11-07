5094c4e818f3581876dafc0a1b9785245c6311cf
Li Yang <yangl395@newschool.edu>
2015-11-07 18:46:06 -0500

    Train set2

--- TrainGame/Assets/trainShaking.cs
+++ TrainGame/Assets/trainShaking.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class trainShaking : MonoBehaviour {
 
 	// Use this for initialization
+	public float speed;
 	void Start () {
 		op = GameObject.Find ("train").transform.position;
 		op2 = GameObject.Find ("train2").transform.position;
 		op.x -= 10f;
 		op2.x -= 10f;
+		speed = 0.04f;
 	}
 	private Vector3 op;
 	private Vector3 op2;
@@ -19,8 +21,13 @@ public class trainShaking : MonoBehaviour {
 		}
 		if (op2.x < -9.47f) {
 			op2.x += 0.05f;
+		}else{
+			GameObject.Find ("maintrack").transform.Translate(Vector3.left*speed);
+			if(GameObject.Find ("maintrack").transform.position.x<=-12.4f){
+				GameObject.Find ("maintrack").transform.Translate(Vector3.right*12.4f);
+			}
 		}
-		GameObject.Find ("train").transform.position = op + Random.Range(0f,0.1f) * Vector3.up;
-		GameObject.Find ("train2").transform.position = op2 + Random.Range(0f,0.1f) * Vector3.up;
+		GameObject.Find ("train").transform.position = op + Random.Range(0f,0.05f) * Vector3.up;
+		GameObject.Find ("train2").transform.position = op2 + Random.Range(0f,0.05f) * Vector3.up;
 	}
 }
