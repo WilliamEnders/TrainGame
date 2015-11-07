9c26d06fb9f987ccbcb40a7e3899c57f2676c870
William Enders <14hourlunchbreak@gmail.com>
2015-11-06 23:32:45 -0500

    Added Character Sprite

--- TrainGame/Assets/player.cs
+++ TrainGame/Assets/player.cs
@@ -16,14 +16,28 @@ public class player : MonoBehaviour {
 	private float ground;
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("w")) transform.Translate(Vector3.forward*speed);
-		else if (Input.GetKey("s")) transform.Translate(Vector3.back*speed);	
-		if (Input.GetKey ("d")) transform.Translate(Vector3.right*speed);
-		else if (Input.GetKey("a")) transform.Translate(Vector3.left*speed);	
+		if (Input.GetKey ("w")){ 
+			transform.Translate(transform.forward*speed);
+		}
+		else if (Input.GetKey("s")) {
+			transform.Translate(-transform.forward*speed);
+		}
+		if (Input.GetKey ("d")) {
+			transform.Translate (transform.right * speed); 
+			transform.rotation = new Quaternion (0, 180, 0, 0);
+		} else if (Input.GetKey ("a")) {
+			transform.Translate (-transform.right * speed); 
+			transform.rotation = new Quaternion (0, 0, 0, 0);
+		}
 		if (Input.GetKeyDown ("space")) {
 			if(transform.position.y<=ground + GameObject.Find ("train").transform.position.y-oy){
 				GetComponent<Rigidbody>().AddForce(Vector3.up*jumpspeed);
 			}
+		}
+		if (Input.GetKey ("w") || Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d")) {
+			GetComponent<Animator> ().Play ("walk");
+		} else {
+			GetComponent<Animator> ().Play ("idle");
 		}
 	}
 }
