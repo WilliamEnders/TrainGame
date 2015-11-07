24d8720f44d92852f7b8d21aacc041d59081c8d8
Li Yang <yangl395@newschool.edu>
2015-11-06 22:58:01 -0500

    Basic Train

---
+++ TrainGame/Assets/player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.1f;
+		jumpspeed = 600f;
+		ground = -1.20f;
+		oy = GameObject.Find ("train").transform.position.y;
+	}
+	private float oy;
+	private float speed;
+	private float jumpspeed;
+	private float ground;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w")) transform.Translate(Vector3.forward*speed);
+		else if (Input.GetKey("s")) transform.Translate(Vector3.back*speed);	
+		if (Input.GetKey ("d")) transform.Translate(Vector3.right*speed);
+		else if (Input.GetKey("a")) transform.Translate(Vector3.left*speed);	
+		if (Input.GetKeyDown ("space")) {
+			if(transform.position.y<=ground + GameObject.Find ("train").transform.position.y-oy){
+				GetComponent<Rigidbody>().AddForce(Vector3.up*jumpspeed);
+			}
+		}
+	}
+}
