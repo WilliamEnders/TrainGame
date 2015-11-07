06c3c236fe3bbef7c70fe3775002aeec1e4abb38
William Enders <14hourlunchbreak@gmail.com>
2015-11-07 16:37:10 -0500

    4 Characters

--- TrainGame/Assets/player.cs
+++ TrainGame/Assets/player.cs
@@ -2,39 +2,48 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
-
+	
+	private float oy;
+	public float speed;
+	private float jumpspeed;
+	private float ground;
+	
+	public string up;
+	public string down;
+	public string left;
+	public string right;
+	public string action;
+	public string action2;
+	
 	// Use this for initialization
 	void Start () {
-		speed = 0.1f;
 		jumpspeed = 600f;
 		ground = -1.20f;
 		oy = GameObject.Find ("train").transform.position.y;
+		
 	}
-	private float oy;
-	private float speed;
-	private float jumpspeed;
-	private float ground;
+	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("w")){ 
-			transform.Translate(transform.forward*speed);
+		if (Input.GetKey (up)){ 
+			transform.Translate(transform.forward * speed * Time.deltaTime);
 		}
-		else if (Input.GetKey("s")) {
-			transform.Translate(-transform.forward*speed);
+		else if (Input.GetKey(down)) {
+			transform.Translate(-transform.forward * speed * Time.deltaTime);
 		}
-		if (Input.GetKey ("d")) {
-			transform.Translate (transform.right * speed); 
+		if (Input.GetKey (right)) {
+			transform.Translate (transform.right * speed * Time.deltaTime); 
 			transform.rotation = new Quaternion (0, 180, 0, 0);
-		} else if (Input.GetKey ("a")) {
-			transform.Translate (-transform.right * speed); 
+		} else if (Input.GetKey (left)) {
+			transform.Translate (-transform.right * speed * Time.deltaTime); 
 			transform.rotation = new Quaternion (0, 0, 0, 0);
 		}
-		if (Input.GetKeyDown ("space")) {
+		if (Input.GetKeyDown (action2)) {
 			if(transform.position.y<=ground + GameObject.Find ("train").transform.position.y-oy){
 				GetComponent<Rigidbody>().AddForce(Vector3.up*jumpspeed);
 			}
 		}
-		if (Input.GetKey ("w") || Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d")) {
+		if (Input.GetKey (up) || Input.GetKey (down) || Input.GetKey (right) || Input.GetKey (left)) {
 			GetComponent<Animator> ().Play ("walk");
 		} else {
 			GetComponent<Animator> ().Play ("idle");
