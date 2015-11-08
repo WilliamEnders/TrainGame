f845787f082e4f3339d5232cb1a405308d057839
Li Yang <yangl395@newschool.edu>
2015-11-07 21:35:21 -0500

    Sound added

--- TrainGame/Assets/trainShaking.cs
+++ TrainGame/Assets/trainShaking.cs
@@ -11,16 +11,27 @@ public class trainShaking : MonoBehaviour {
 		op.x -= 10f;
 		op2.x -= 10f;
 		speed = 0.24f;
-		bump
+		bump = 1f;
 	}
 	private Vector3 op;
 	private Vector3 op2;
 	private float bump;
 	// Update is called once per frame
 	public void trainBump(){
-
+		bump = 20;
+		Vector3 _pos = Vector3.down*2f + (GameObject.Find ("p1").transform.position + GameObject.Find ("p2").transform.position + GameObject.Find ("p3").transform.position +GameObject.Find ("p4").transform.position)/4f;
+		GameObject.Find ("p1").GetComponent<Rigidbody> ().AddExplosionForce (1000f, _pos, 10f);
+		GameObject.Find ("p2").GetComponent<Rigidbody> ().AddExplosionForce (1000f, _pos, 10f);
+		GameObject.Find ("p3").GetComponent<Rigidbody> ().AddExplosionForce (1000f, _pos, 10f);
+		GameObject.Find ("p4").GetComponent<Rigidbody> ().AddExplosionForce (1000f, _pos, 10f);
+		GameObject.Find ("coal").GetComponent<Rigidbody> ().AddExplosionForce (1000f, _pos, 10f);
 	}
 	void Update () {
+		if (bump > 1f) {
+			bump -= 0.4f;
+		} else {
+			bump = 1f;
+		}
 		if (op.x < -0.9f) {
 			op.x += 0.05f;
 		}
@@ -36,6 +47,6 @@ public class trainShaking : MonoBehaviour {
 			}
 		}
 		GameObject.Find ("train").transform.position = op + Random.Range(0f,0.05f) * Vector3.up*bump;
-		GameObject.Find ("train2").transform.position = op2 + Random.Range(0f,0.05f) * Vector3.up;
+		GameObject.Find ("train2").transform.position = op2 + Random.Range(0f,0.05f) * Vector3.up*bump;
 	}
 }
