dd46b4a4a3b72429da0c5a55eefe7ddc9a056ecc
William Enders <endew231@newschool.edu>
2015-12-16 17:57:11 -0500

    instructions screen

---
+++ TrainGame/Assets/instructions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class instructions : MonoBehaviour {
+
+	private GameObject inst;
+	// Use this for initialization
+	void Start () {
+		inst = GameObject.Find ("instructions");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void reveal(){
+		inst.GetComponent<Animator> ().Play ("instructions up");
+	}
+	public void hide(){
+		inst.GetComponent<Animator> ().Play ("instructions down");
+	}
+}
---
+++ TrainGame/Assets/loadLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Application.GetStreamProgressForLevel("level1") == 1){
+			Application.LoadLevel("level1");
+		}
+	}
+}
